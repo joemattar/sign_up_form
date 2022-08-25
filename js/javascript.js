d1088adb50f929cce5a00fb00d1b46fd67b80dda
Joe M <joemattar@gmail.com>
2022-08-25 18:24:07 -0400

    Add phone input masks functionality

--- js/javascript.js
+++ js/javascript.js
@@ -68,3 +68,81 @@ inputEmail.addEventListener("input", () => {
 submitButton.addEventListener("click", () => {
     spanFirstName.textContent = "TESTING SUBMIT BUTTON";
 })
+
+
+// Add event listener when the HTML document has been completely parsed, and all deferred scripts have downloaded
+document.addEventListener('DOMContentLoaded', () => {
+    // for all element that have an attribute placeholder and an attribute data-masks
+    for (const el of document.querySelectorAll("[placeholder][data-slots]")) {
+        // declare pattern as placeholder value
+        const pattern = el.getAttribute("placeholder");
+        // declare a variable slots to fetch unique data-slots symbols
+        const slots = new Set(el.dataset.slots || "_");
+
+
+        // const prev = (j => Array.from(pattern, (elem,index) => slots.has(elem) ? j = index + 1 : j ))(0);
+        const prev = Array.from(pattern, (elem,index) => {
+                if (slots.has(elem)) {
+                    return index + 1;
+                } else {
+                    return 0;
+                }
+            });
+        // REFINE ABOVE!!
+        console.log(prev)
+
+        // Find the index in pattern's array that matched the first instance of data-slots symbol
+        const first = [...pattern].findIndex(c => slots.has(c));
+
+        // Regex constructor depending on the element data-accept attribute
+        const accept = new RegExp(el.dataset.accept || "\\d", "g");
+
+        const clean = input => {
+                input = input.match(accept) || [];
+                return Array.from(pattern, c =>
+                    input[0] === c || slots.has(c) ? input.shift() || c : c
+                );
+            };
+
+        const format = () => {
+                const [i, j] = [el.selectionStart, el.selectionEnd].map(i => {
+                    i = clean(el.value.slice(0, i)).findIndex(c => slots.has(c));
+                    return i<0? prev[prev.length-1]: back? prev[i-1] || first: i;
+                });
+                el.value = clean(el.value).join``;
+                el.setSelectionRange(i, j);
+                back = false;
+            };
+        
+        // Event listener changes {back} to true when backspace key is pressed
+        let back = false;
+        el.addEventListener("keydown", (e) => {
+            back = e.key === "Backspace";
+        });
+
+        el.addEventListener("input", format);
+        el.addEventListener("focus", format);
+
+        // The blur event fires when an element has lost focus ~focusout // focusout bubbles while blur does not.
+        // Check at blur if field matches placeholder exactly and deletes input
+        el.addEventListener("blur", () => {
+            if (el.value === pattern) {
+                el.value = "";
+        }});
+    }
+});
+
+
+// HTML Examples for Masks
+{/*
+Date time: 
+<input placeholder="dd/mm/yyyy hh:mm" data-slots="dmyh">
+Telephone:
+<input placeholder="+1 (___) ___-____" data-slots="_">
+MAC Address:
+<input placeholder="XX:XX:XX:XX:XX:XX" data-slots="X" data-accept="[\dA-H]">
+Alphanumeric:
+<input placeholder="__-__-__-____" data-slots="_" data-accept="\w" size="13">
+Credit Card:
+<input placeholder=".... .... .... ...." data-slots="." data-accept="\d" size="19">
+*/}
